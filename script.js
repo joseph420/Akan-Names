af70cfa3d577287bdeb2e2dd40c204b4c279bd66
joseph <cr.muturi@gmail.com>
2019-10-25 14:49:39 +0300

    add in javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+var maleAkanName = [
+ "Kwasi",
+"Kwadwo",
+"Kwabena",
+"Kwaku",
+"Yaw",
+"Kofi",
+  ];
+  var femaleAkanName = [
+"Akosua",
+"Adwoa",
+"Abenaa",
+"Akua",
+"Yaa",
+"Afua",
+"Ama",
+  ];
+  var daysOfTheWeek = [
+"Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",
+  ];
